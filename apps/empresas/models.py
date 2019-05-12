7182ea7629e91e9e9b9e862313c5853ce501f256
Luis Marra <python.luismarra@gmail.com>
2019-05-12 15:45:04 -0300

    first commit + primeiro app e primeiro model

---
+++ apps/empresas/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Empresa(models.Model):
+    nome = models.CharField(max_length=100, help_text='Nome da empresa')
+    segmento = models.CharField(max_length=200, help_text='Segmento da empresa')
+
+    def __str__(self):
+        return self.nome + ': ' + self.segmento
